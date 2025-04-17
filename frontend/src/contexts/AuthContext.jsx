b8bd8648f20e7a4d494d4398e7f311c822742c1f
KashishMaurya <kashish.social.18@gmail.com>
2025-04-17 19:30:39 +0530

    authcontext

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useState } from "react";
+import axios from "axios";
+import HttpStatus from "http-status";
+import { useNavigate } from "react-router-dom";
+
+// Create context
+export const AuthContext = createContext({});
+
+// Axios client
+const client = axios.create({
+  baseURL: "http://localhost:8080/api/v1/users",
+});
+
+// Provider component
+export const AuthProvider = ({ children }) => {
+  const [userData, setUserData] = useState(null);
+
+  const router = useNavigate();
+
+  const handleRegister = async (name, username, password) => {
+    try {
+      const request = await client.post("/register", {
+        name,
+        username,
+        password,
+      });
+
+      if (request.status === HttpStatus.CREATED) {
+        return request.data.message;
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+      throw error;
+    }
+  };
+
+  const handleLogin = async (username, password) => {
+    try {
+      const request = await client.post("/login", {
+        username,
+        password,
+      });
+
+      if (request.status === HttpStatus.OK) {
+        localStorage.setItem("token", request.data.token);
+        // setUserData(request.data.user); 
+        // router("/dashboard"); 
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      throw error;
+    }
+  };
+
+  const data = {
+    userData,
+    setUserData,
+    handleRegister,
+    handleLogin, 
+  };
+
+  return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+};
--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -7,22 +7,41 @@ import {
   Grid,
   // Link,
   Paper,
+  Snackbar,
   TextField,
   // Typography,
 } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Authentication = () => {
-
   const [username, setUsername] = React.useState();
   const [password, setPassword] = React.useState();
   const [name, setName] = React.useState();
   const [error, setError] = React.useState();
   const [messages, setMessages] = React.useState();
 
-  const[formState, setFormState] = React.useState(0);
+  const [formState, setFormState] = React.useState(0);
 
   const [open, setOpen] = React.useState(false);
+
+  const { handleRegister, handleLogin } = React.useContext(AuthContext);
+
+  let handleAuth = async () => {
+    try {
+      if (formState === 0) {
+      }
+      if (formState === 1) {
+        let result = await handleRegister(name, username, password);
+        console.log(result);
+        setMessages(result);
+        setOpen(true);
+      }
+    } catch (error) {
+      let message = error.response.data.message;
+      setError(message);
+    }
+  };
 
   const paperStyle = {
     padding: 20,
@@ -32,7 +51,7 @@ const Authentication = () => {
   };
 
   const avatarStyle = { backgroundColor: "#7b1fa2" };
-  
+
   const btnStyle = { margin: "8px 0" };
 
   return (
@@ -67,34 +86,72 @@ const Authentication = () => {
         justifyContent="center"
       >
         <div style={paperStyle}>
-          <Grid align="center">
+          <Grid align="center" sx={{ mb: 2 }}>
             <Avatar style={avatarStyle}>
               <LockOutlinedIcon />
             </Avatar>
-            <h2>Sign In</h2>
           </Grid>
 
+          <div align="center">
+            <Button
+              variant={formState === 0 ? "contained" : ""}
+              onClick={() => {
+                setFormState(0);
+              }}
+            >
+              Sign In
+            </Button>
+            <Button
+              variant={formState === 1 ? "contained" : ""}
+              onClick={() => {
+                setFormState(1);
+              }}
+            >
+              Sign Up
+            </Button>
+          </div>
+
+          {formState === 1 ? (
+            <TextField
+              label="Full Name"
+              // placeholder="Enter Username"
+              id="username"
+              name="username"
+              fullWidth
+              required
+              margin="normal"
+              autoFocus
+              onChange={(e) => setName(e.target.value)}
+            />
+          ) : (
+            <></>
+          )}
+
           <TextField
-            label="Email Address"
-            placeholder="Enter email"
+            label="Username"
+            // placeholder="Enter Username"
+            id="username"
+            name="username"
             fullWidth
             required
             margin="normal"
+            autoFocus
+            onChange={(e) => setUsername(e.target.value)}
           />
 
           <TextField
             label="Password"
-            placeholder="Enter password"
+            // placeholder="Enter Password"
             type="password"
+            id="password"
+            name="password"
             fullWidth
             required
             margin="normal"
+            onChange={(e) => setPassword(e.target.value)}
           />
 
-          <FormControlLabel
-            control={<Checkbox name="checkedB" color="primary" />}
-            label="Remember me"
-          />
+          <p style={{color: "red"}}>{error}</p>
 
           <Button
             type="button"
@@ -102,13 +159,17 @@ const Authentication = () => {
             variant="contained"
             fullWidth
             style={btnStyle}
+            onClick={handleAuth}
           >
-            Sign in
+            {formState === 0 ? "Login" : "Register"}
           </Button>
         </div>
       </Grid>
+
+      <Snackbar open={open} autoHideDuration={4000} message={messages} />
     </Grid>
   );
 };
 
 export default Authentication;
+
