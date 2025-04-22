5a5d41f7f920e55e62c5b2b24d660dae84abbe7d
KashishMaurya <kashish.social.18@gmail.com>
2025-04-22 17:51:27 +0530

    audio video setup

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
 import "../styles/VideoComponent.css";
-import { useState, useRef, useEffect } from "react";
-
+import { Badge, IconButton, TextField, Button } from "@mui/material";
 
 const server_url = "httlp://localhost:8080";
 
@@ -49,5 +49,118 @@ export default function VideoMeetComponent() {
   //     alert("Please use Chrome browser for better experience");
   // }
 
-  return <div>VideoMeetComponent</div>;
+  const getPermissions = async () => {
+    try {
+      //video
+      const videoPermission = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+
+      if (videoPermission) {
+        setVideoAvailable(true);
+      } else {
+        setVideoAvailable(false);
+      }
+
+      //audio
+      const audioPermission = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+
+      if (audioPermission) {
+        setAudioAvailable(true);
+      } else {
+        setAudioAvailable(false);
+      }
+
+      //screen sharing
+      if (navigator.mediaDevices.getDisplayMedia) {
+        setScreenAvailable(true);
+      } else {
+        setScreenAvailable(false);
+      }
+
+      //stream
+      if (videoAvailable || audioAvailable) {
+        const userMediaStream = await navigator.mediaDevices.getUserMedia({
+          video: videoAvailable,
+          audio: audioAvailable,
+        });
+
+        if (userMediaStream) {
+          window.localStream = userMediaStream;
+          if (localVideoRef.current) {
+            localVideoRef.current.srcObject = userMediaStream;
+          }
+        }
+      }
+    } catch (error) {
+      console.log("Error accessing media devices:", error);
+    }
+  };
+
+  useEffect(() => {
+    getPermissions();
+  }, []);
+
+  let getUserMediaSuccess = (stream) => {};
+
+  let getUserMedia = () => {
+    if ((video && videoAvailable) || (audio && audioAvailable)) {
+      navigator.mediaDevices
+        .getUserMedia({ video: video, audio: audio })
+        .then(getUserMediaSuccess)
+        .then((stream) => {})
+        .catch((e) => console.log(e));
+    } else {
+      try {
+        let tracks = localVideoRef.current.srcObject.getTracks();
+        tracks.forEach((track) => track.stop());
+      } catch (error) {}
+    }
+  };
+
+  useEffect(() => {
+    if (video !== undefined && audio !== undefined) {
+      getUserMedia();
+    }
+  }, [video, audio]);
+
+  let getMedia = () => { 
+    setVideo(videoAvailable);
+    setAudio(audioAvailable);
+
+    // connectToSocketServer();
+  };
+
+  let connect = () => {
+    console.log("connecting....");
+    setAskForUsername(false);
+    getMedia();
+  };
+
+  return (
+    <div>
+      {askForUsername === true ? (
+        <div>
+          <h1>Enter the lobby</h1>
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            variant="outlined"
+          />
+          <Button variant="contained" onClick={connect}>
+            Connect
+          </Button>
+
+          <br />
+          <video ref={localVideoRef} autoPlay muted></video>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
